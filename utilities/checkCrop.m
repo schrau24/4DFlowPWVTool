4f005fac357bcf4de69d91ac419ab0358b38b1a8
Eric Schrauben <schrau24@gmail.com>
2021-04-08 16:54:06 +0200

    update crop function

--- utilities/checkCrop.m
+++ utilities/checkCrop.m
@@ -1,12 +1,12 @@
 function choice = checkCrop
 answer = questdlg('Is this correct?', ...
 	'Check cropped image', ...
-	'Yes, continue','Cancel Cropping', 'No','Yes, continue');
+	'Yes, continue','Cancel Cropping', 'No, retry','Yes, continue');
 % Handle response
 switch answer
     case 'Yes, continue'
         choice = 1;
-    case 'No'
+    case 'No, retry'
         choice = 0;
     case 'Cancel Cropping'
         choice = 2;
