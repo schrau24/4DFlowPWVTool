ed2d1cc441c1d46579b0bdb45f8964de82eab55c
Schrauben <emschrauben@amc.intra>
2021-09-03 09:07:54 +0200

    Updates to wavelet PWV calculation

--- utilities/calc_pwv.m
+++ utilities/calc_pwv.m
@@ -1,6 +1,10 @@
 function [D, fitObject, dist_total] = calc_pwv(waveforms,dist_total, timeres, PWVcalctype, scale)
 
 % PWV calc type: 1 is cross correlation, 2 is TTF, 3 is Wavelet
+% normalize waveforms, save scalingFactor to update delays appropriately
+[waveforms, ~, scalingFactor] = zscore(waveforms');
+scalingFactor = scalingFactor(1)./scalingFactor(2:end);
+waveforms = waveforms';
 
 % to interp to 1/scale ms, we need how many frames?
 nFrames = size(waveforms,2);
@@ -10,17 +14,14 @@ nFrames_interp = floor(timeres/scale*nFrames);
 x = 1:nFrames;
 xq = linspace(1,nFrames,nFrames_interp);
 METHOD = 'pchip';
-plot_steps = true;
+plot_steps = false;
 
 % grab first flow waveform
-flow_sl1 = interp1(x,waveforms(1,:),xq,METHOD);
+flow_sl1 = smooth(interp1(x,waveforms(1,:),xq,METHOD),25);
+
 if min(flow_sl1) < 0
     flow_sl1 = flow_sl1 + abs(min(flow_sl1));
 end
-if min(flow_sl1) > 0
-    flow_sl1 = flow_sl1 - (min(flow_sl1));
-end
-
 % normalize
 flow_sl1 = flow_sl1./max(abs(flow_sl1));
 
@@ -34,6 +35,9 @@ flow_sl1 = circshift(flow_sl1, midPt-indMax1);
 % 20% of max, first to the left
 indStart = max(find(flow_sl1(1:midPt) < 0.2*maxFl)) + 1;
 indEnd   = max(find(flow_sl1(indStart:midPt) < 0.8*maxFl)) + indStart -1;
+
+% indStart = max(find(flow_sl1(1:indMax1) < 0.2*maxFl)) + 1;
+% indEnd   = max(find(flow_sl1(indStart:indMax1) < 0.8*maxFl)) + indStart -1;
 pts1 = indStart:indEnd;
 
 if PWVcalctype == 2     % TTF
@@ -42,25 +46,21 @@ if PWVcalctype == 2     % TTF
 end
 
 % for wavelet
-[y1,PERIOD,SCALE,COI,DJ, PARAMOUT, K] = contwt(flow_sl1,.001,1,.125,[],[],'dog',4);
+[y1,PERIOD,SCALE,COI,DJ, PARAMOUT, K] = contwt(flow_sl1,0.001,1,[],[],[],'dog',4);
 f1 = 1./PERIOD;
 ind = [];
 % loop over all flow curves
 for slice = 2:size(waveforms,1)
     
     % second flow waveform, circshift by same amount as first waveform
-    flow_sl2 = circshift(interp1(x,waveforms(slice,:),xq,METHOD), midPt-indMax1);
-    
+    flow_sl2 = smooth(circshift(interp1(x,waveforms(slice,:),xq,METHOD), midPt-indMax1),25);
+%     flow_sl2 = smooth(interp1(x,waveforms(slice,:),xq,METHOD));
     if min(flow_sl2) < 0
         flow_sl2 = flow_sl2 + abs(min(flow_sl2));
     end
-    if min(flow_sl2) > 0
-        flow_sl2 = flow_sl2 - (min(flow_sl2));
-    end
-    
-    %     normalize
+    % normalize
     flow_sl2 = flow_sl2./max(abs(flow_sl2));
-    
+  
     % find the systolic upslope (20% < x < 80%) at or after maxInd of
     % flow_sl1
     [maxFl, indMax2] = max(flow_sl2);
@@ -93,24 +93,29 @@ for slice = 2:size(waveforms,1)
             D(slice) = -fitObject(2) / fitObject(1);
             
         case 3      % Wavelet time delay estimation
-            [y2,~,~,~,~,~,~] = contwt(flow_sl2,.001,1,.125,[],[],'dog',4);
-            % the complex cross-spectrum
-            y = y1.*conj(y2);
-            
-            % find frequencies 1:10 Hz and keep only those
-            ind_f = find(f1 >= 1 & f1 <= 10);
-            
+            [y2,~,~,~,~,~,~] = contwt(flow_sl2,0.001,1,[],[],[],'dog',4);
+
+            % cross-spectrum is only performed on 
+            % 1) points during systole
+            % 2) over frequencies as between fc and 10 Hz
             % the points between foot of flow_sl1 and peak of flow_sl2
             pts = unique([pts1, pts2]);
+            fc = 1/numel(pts);     % in Hz, the fundamental freq
+            ind_f = find(f1 >= fc & f1 <= 10);
             
+            % the complex cross-spectrum
+            y = y1(ind_f,pts).*conj(y2(ind_f,pts));
+              
             % calculate y_hat
             y_hat = y/(sum(sum(abs(y))));
             
-            psi = atan(imag(y))./repmat(f1'*2*pi,[1 size(y,2)]);
-            aa =    dot(y_hat(ind_f,pts),psi(ind_f,pts));  % eqn 7 in Bargiotas paper
-            
-%             tempDelay = abs(sum(aa(:)))*1000*4 * scale;
+            psi =   atan2(imag(y),real(y))./repmat(f1(ind_f)'*2*pi,[1 length(pts)]);
+            aa =    dot(y_hat,psi);  % eqn 7 in Bargiotas paper
             tempDelay = abs(sum(aa(:)))*1000 * scale;
+            
+            % re-scale delay from zscore
+            tempDelay = tempDelay/(scalingFactor(slice-1));
+            
             if (dist_total(slice-1) > 20 && tempDelay < 1) || ...
                     (dist_total(slice-1) > 40 && tempDelay < 2) || ...
                     (dist_total(slice-1) > 60 && tempDelay < 3) || ...
@@ -119,12 +124,12 @@ for slice = 2:size(waveforms,1)
                 % force the delays to make sense along vessel
                 ind = cat(1,ind,slice);     % to remove at the end for fitting
             end
-            D(slice - 1) = tempDelay;
             
+            D(slice - 1) = tempDelay;
     end
     
     
-    if plot_steps && mod(slice,10) == 0
+    if plot_steps && mod(slice,10) == 2
         figure(400); clf;
         plot(flow_sl1,'r'); hold on;
         plot(pts1,flow_sl1(pts1),'r*')
@@ -137,7 +142,7 @@ for slice = 2:size(waveforms,1)
     end
     
 end
-% % remove unused slices
+% remove unused slices
 D(ind-1) = [];
 dist_total(ind-1) = [];
 
